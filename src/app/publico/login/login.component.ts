ea9e30d39e022868ec8a43fcada2ee68de4de288
Atila Augusto dos Santos <atila.sistemas@gmail.com>
2019-11-04 11:15:50 -0300

    codigo login component

--- src/app/publico/login/login.component.ts
+++ src/app/publico/login/login.component.ts
@@ -1,14 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {Storage} from '@ionic/storage';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: Router,
+    private storage: Storage
+  ) {
+    this.buildForm();
+  }
 
   ngOnInit() {}
+  // Cria instancia do form
+  buildForm() {
+    this.form = this.formBuilder.group({
+      email: ['', Validators.required],
+      pass: ['', Validators.required]
+    });
+  }
 
+  // vai para página de cadastro
+  cadastro() {
+    this.route.navigate(['/publico/cadastro']);
+  }
+  // faz o login e vai para a pagina home
+  async login() {
+    await this.storage.set('usuario', 'OK');
+    this.route.navigate(['/home']);
+  }
 }
