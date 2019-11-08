2c70aacb1903e6cc6c4b21e6ca7ca2262ef771b9
Atila Augusto dos Santos <atila.sistemas@gmail.com>
2019-11-08 09:43:12 -0300

    service api

---
+++ src/app/api.service.ts
@@ -0,0 +1,46 @@
+import {environment} from './../environments/environment';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams, HttpHeaders} from '@angular/common/http';
+import {throwError, Observable} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  private formatErros(error: any) {
+    return throwError(error.error);
+  }
+
+  get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
+    return this.http
+      .get(`${environment.url_api}${path}`, {params})
+      .pipe(catchError(this.formatErros));
+  }
+
+  post(
+    path: string,
+    body: any = {},
+    headers: HttpHeaders = new HttpHeaders()
+  ): Observable<any> {
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    return this.post(`${environment.url_api}${path}`, body, headers).pipe(
+      catchError(this.formatErros)
+    );
+  }
+
+  put(
+    path: string,
+    body: any = {},
+    headers: HttpHeaders = new HttpHeaders()
+  ): Observable<any> {
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    return this.put(`${environment.url_api}${path}`, body, headers).pipe(
+      catchError(this.formatErros)
+    );
+  }
+}
