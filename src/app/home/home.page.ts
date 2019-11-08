be7e1f67e71a7d01d4a5f1dcb44c3e2b0276aa69
Atila Augusto dos Santos <atila.sistemas@gmail.com>
2019-11-08 11:48:37 -0300

    finalização tela home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {Storage} from '@ionic/storage';
 import {RestautanteService} from '../services/restautante.service';
+import {Restaurante} from '../model/restaurante.model';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,7 @@ import {RestautanteService} from '../services/restautante.service';
   styleUrls: ['home.page.scss']
 })
 export class HomePage implements OnInit {
-  constructor(
-    private route: Router,
-    private storage: Storage,
-    private restauranteService: RestautanteService
-  ) {}
-
+  restaurantes: Restaurante[];
   slideOptions = {
     autoplay: true,
     speed: 1000,
@@ -22,9 +18,16 @@ export class HomePage implements OnInit {
       maxRatio: 5
     }
   };
+  constructor(
+    private route: Router,
+    private storage: Storage,
+    private restauranteService: RestautanteService
+  ) {}
 
   ngOnInit(): void {
-    this.restauranteService.list().subscribe(result => console.log(result));
+    this.restauranteService
+      .list()
+      .subscribe(result => (this.restaurantes = result));
 
     this.storage.get('usuario').then(result => {
       if (!result) {
